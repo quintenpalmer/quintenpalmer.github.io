f7b44c6b2799970a2b8fc7ef2dc5440c9e551866
Quinten Palmer <quintenpalmer@protonmail.com>
2023-01-20 23:53:02 -0500

    Leverage `Library::from_library_directory` in main
    This is a simple "tree" display chart that can be run over a directory
    full of flac files.

--- codeexamples/2022-01-29-parsing/src/main.rs
+++ codeexamples/2022-01-29-parsing/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 mod parse;
 mod scan;
 mod organize;
@@ -5,5 +7,23 @@ mod model;
 pub mod impls;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Let's read some audio metadata");
+    let args: Vec<String> = env::args().collect();
+
+    println!("Let's build the library by parsing all of the files");
+    let library = model::Library::from_library_directory(args[1].clone()).unwrap();
+
+    println!("Now let's print all of the tracks we found");
+    for artist in library.artists.values() {
+        println!("\tArtist: {}", artist.name);
+        for album in artist.albums.values() {
+            println!("\t\tAlbum: {}", album.name);
+            for disc in album.discs.values() {
+                println!("\t\t\tDisc: {}", disc.number);
+                for track in disc.tracks.values() {
+                    println!("\t\t\t\tTrack: {}", track.track_title);
+                }
+            }
+        }
+    }
 }
