d46f69a7ffa4854f25a1d3e7d765ce446fcf47e4
Quinten Palmer <quintenpalmer@protonmail.com>
2023-01-22 21:44:18 -0500

    Rename to `audio_files`
    It's not actually a metadata map :facepalm:.

--- codeexamples/2022-01-29-parsing/src/scan.rs
+++ codeexamples/2022-01-29-parsing/src/scan.rs
@@ -4,21 +4,21 @@ use std::path;
 use crate::{model, util};
 
 pub fn find_audio_files(scan_path: &path::PathBuf) -> Result<Vec<path::PathBuf>, model::Error> {
-    let mut metadata_map = Vec::new();
+    let mut audio_files = Vec::new();
 
     for child_entry in fs::read_dir(scan_path)? {
         let child_entry = child_entry?;
         let child_path = child_entry.path();
         if child_entry.file_type()?.is_dir() {
-            metadata_map.append(&mut find_audio_files(&child_path)?);
+            audio_files.append(&mut find_audio_files(&child_path)?);
         }
         if child_entry.file_type()?.is_file() {
             let maybe_extension = util::get_maybe_extension_string(&child_path);
 
             match maybe_extension {
                 Some(extension) => match extension.as_str() {
-                    "flac" => metadata_map.push(child_path),
-                    "mp3" => metadata_map.push(child_path),
+                    "flac" => audio_files.push(child_path),
+                    "mp3" => audio_files.push(child_path),
                     _ => println!(
                         "DEBUG: Skipping file with unknown extension: {}",
                         child_path.to_string_lossy()
@@ -32,5 +32,5 @@ pub fn find_audio_files(scan_path: &path::PathBuf) -> Result<Vec<path::PathBuf>,
         }
     }
 
-    Ok(metadata_map)
+    Ok(audio_files)
 }
