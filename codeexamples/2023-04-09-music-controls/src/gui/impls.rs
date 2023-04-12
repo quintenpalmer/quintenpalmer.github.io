d418aabf3e957b2cdfa9c46e50bd40613bfc8b38
Quinten Palmer <quintenpalmer@protonmail.com>
2023-04-11 22:01:23 -0400

    Introduce plumbing for future subscription work
    This is what will actually let the iced GUI talk with the sink backend.

--- codeexamples/2023-04-09-music-controls/src/gui/impls.rs
+++ codeexamples/2023-04-09-music-controls/src/gui/impls.rs
@@ -1,6 +1,9 @@
+use std::cell;
+
 use iced;
 
 use crate::datastore;
+use crate::sink;
 
 use super::{message, state, update, view};
 
@@ -8,11 +11,16 @@ impl iced::Sandbox for state::State {
     type Message = message::Message;
 
     fn new() -> Self {
+        let (sink_sender, sink_recv) = sink::create_backend_with_client_and_callback();
         state::State {
             page: state::Page::Home,
             datastore: datastore::model::Library::from_library_directory(".").unwrap(),
             playback: state::PlaybackInfo {
                 currently_playing: None,
+            },
+            sink: state::Sink {
+                sink_message_sender: sink_sender,
+                sink_callback_recv: cell::RefCell::new(Some(sink_recv)),
             },
         }
     }
--- codeexamples/2023-04-09-music-controls/src/gui/state.rs
+++ codeexamples/2023-04-09-music-controls/src/gui/state.rs
@@ -1,9 +1,14 @@
+use std::cell;
+use std::sync::mpsc;
+
 use crate::datastore;
+use crate::shared;
 
 pub struct State {
     pub page: Page,
     pub datastore: datastore::model::Library,
     pub playback: PlaybackInfo,
+    pub sink: Sink,
 }
 
 pub enum Page {
@@ -11,6 +16,11 @@ pub enum Page {
     ArtistList,
     ArtistAlbumList(String),
     AlbumTrackList(String, String),
+}
+
+pub struct Sink {
+    pub sink_message_sender: mpsc::Sender<shared::SinkMessage>,
+    pub sink_callback_recv: cell::RefCell<Option<mpsc::Receiver<shared::SinkCallbackMessage>>>,
 }
 
 pub struct PlaybackInfo {
