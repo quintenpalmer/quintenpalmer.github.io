29956b3cab224554f6631241ad22ca5d060e4bf9
Quinten Palmer <quintenpalmer@protonmail.com>
2023-04-11 23:53:54 -0400

    Display playback info (played/paused and track title)

--- codeexamples/2023-04-09-music-controls/src/gui/view/mod.rs
+++ codeexamples/2023-04-09-music-controls/src/gui/view/mod.rs
@@ -1,5 +1,5 @@
 use iced;
-use iced::widget::{button, Column, Row, Scrollable};
+use iced::widget::{button, text, Column, Row, Scrollable};
 
 use crate::datastore;
 
@@ -21,11 +21,16 @@ pub fn view_state<'a>(state: &'a state::State) -> iced::Element<'a, message::Mes
         crumb_button_row = crumb_button_row.push(Scrollable::new(crumb_button));
     }
 
+    let playback_info = view_playback_info(&state.playback);
 
     let mut ret = Column::new();
 
     ret = ret.push(crumb_button_row);
     ret = ret.push(body);
+    match playback_info {
+        Some(actual_playback_info) => ret = ret.push(actual_playback_info),
+        None => (),
+    };
     ret.into()
 }
 
@@ -45,5 +50,23 @@ fn view_page<'a>(
         state::Page::AlbumTrackList(ref artist_name, ref album_name) => {
             album::view_album_track_list(artist_name.clone(), album_name.clone(), &datastore)
         }
+    }
+}
+
+fn view_playback_info<'a>(
+    playback: &'a state::PlaybackInfo,
+) -> Option<iced::Element<'a, message::Message>> {
+    match playback.currently_playing {
+        Some((ref track, playing)) => {
+            let mut row = Row::new().spacing(10);
+            if playing {
+                row = row.push(text(" >"));
+            } else {
+                row = row.push(text("||"));
+            }
+            row = row.push(text(track.track_title.clone()));
+            Some(row.into())
+        }
+        None => None,
     }
 }
